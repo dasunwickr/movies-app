36196e74ec890e32495fc9d608d34628c39892f5
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-04-13 17:02:14 +0530

    init commit

---
+++ movies-server/src/test/java/com/dasunwickr/moviesserver/MoviesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasunwickr.moviesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
