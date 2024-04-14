bb6d28d6a49d782ac6a43b7fc81a5cb1afd9a3a8
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-04-14 08:33:37 +0530

    Adding CORS

--- movies-server/src/main/java/com/dasunwickr/moviesserver/MovieController.java
+++ movies-server/src/main/java/com/dasunwickr/moviesserver/MovieController.java
@@ -3,10 +3,7 @@ package com.dasunwickr.moviesserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,11 +15,15 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
+
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/{imdbId}")
     public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String imdbId){
         return new ResponseEntity<Optional<Movie>>(movieService.getMovieById(imdbId), HttpStatus.OK);
